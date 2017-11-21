cdfe64394ffb42f20440375deea0ab81af38e2f8
Nithin Betegeri <nithinpb@gmail.com>
2017-11-21 18:08:39 +0530

    Initial Commit

---
+++ Chek/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chek
+//
+//  Created by Nithin Betegeri on 11/21/17.
+//  Copyright © 2017 Nithin Betegeri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
